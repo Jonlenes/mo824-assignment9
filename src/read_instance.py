10ff7feb76b379398868f9c41aa8450498c28509
Jonlenes <jonlenes@outlook.com>
2020-12-21 08:37:12 -0300

    Read instances update

--- src/read_instance.py
+++ src/read_instance.py
@@ -16,21 +16,25 @@ def read_input(filename):
 
 	# hd
 	text_io.readline()
-	hd = [int(text_io.readline()) for _ in range(D)]
+	hd = np.array([int(text_io.readline()) for _ in range(D)])
 
 	# apd
 	text_io.readline()
-	apd = [list(map(int, text_io.readline().split())) for _ in range(P)]
+	apd = np.array([list(map(int, text_io.readline().split())) for _ in range(P)])
 
 	# rpt
 	text_io.readline()
-	rpt = [list(map(int, text_io.readline().split())) for _ in range(P)]
+	rpt = np.array([list(map(int, text_io.readline().split())) for _ in range(P)])
 
 	return P, D, T, S, H, hd, apd, rpt
 
 def instance_names(folder='instances'):
 	exp = os.path.join(folder, '*.pap')
-	return [path.split(os.path.sep)[-1] for path in glob.glob(exp)]
+	names = [path.split(os.path.sep)[-1] for path in glob.glob(exp)]
+	def cmp(name):
+		match = re.compile(r'P(?P<P>\d+)D(?P<D>\d+)S(?P<S>\d+)').search(name)
+		return int(match.group('P')) + int(match.group('D')) + int(match.group('S'))
+	return sorted(names, key=cmp)
 
 if __name__ == "__main__":
 	for instance in instance_names():
